bdbe83c9ce12b489abff3fd1b2adf41ac0a85086
hwilt <henrywwilt@gmail.com>
2020-11-26 10:00:59 -0500

    set up all the days

---
+++ day15.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <fstream>
+#include <stdio.h>
+
+using namespace std;
+
+//input for the day
+string input;
+
+/*
+ * Gets the input out of the file and into a string
+*/
+string getInput() {
+    string out;
+    ifstream file("input/day15.txt");
+    if (file.is_open()) {
+        string line;
+        while(getline(file, line)){
+            out += line;
+        }
+    }
+    return out;
+}
+
+/*
+ * Part One for the day
+*/
+int partOne(string in) {
+    int ret;
+    //TODO: Fill this in
+    
+    
+    
+    return ret;
+}
+
+/*
+ * Part Twp for the day
+*/
+int partTwo(string in) {
+    int ret;
+    //TODO: Fill this in
+
+
+    return ret;
+}
+
+int main() {
+    input = getInput();
+    cout << partOne(input) << endl;
+    cout << partTwo(input);
+    return 0;
+}
