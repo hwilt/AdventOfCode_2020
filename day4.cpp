93824926d7710ea11838e3433e11a8f9797cf031
hwilt <henrywwilt@gmail.com>
2020-12-04 17:03:34 -0500

    Day 4

--- day4.cpp
+++ day4.cpp
@@ -7,30 +7,39 @@
 using namespace std;
 
 //input for the day
-string input;
-
+string input[25];
+int size = 0;
+string required[7] = {"byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"};
+int resize = 6;
 /*
  * Gets the input out of the file and into a string
 */
-string getInput() {
-    string out;
+void getInput() {
     ifstream file("input/day4.txt");
     if (file.is_open()) {
         string line;
+        string already;
         while(getline(file, line)){
-            out += line;
+            if(line.length() != 1){
+                input[size] += line;
+            }
+            else{
+                size++;
+            }
         }
     }
-    return out;
 }
 
 /*
  * Part One for the day
 */
-int partOne(string in) {
-    int ret;
-    //TODO: Fill this in
-    
+int partOne() {
+    int ret = 0;
+    cout << endl;
+    for(int i = 0; i < size; i++){
+        cout << "input["
+        cout << "input[" << i << "]: " << input[i] << endl;
+    }
     
     
     return ret;
@@ -39,8 +48,8 @@ int partOne(string in) {
 /*
  * Part Twp for the day
 */
-int partTwo(string in) {
-    int ret;
+int partTwo() {
+    int ret = 0;
     //TODO: Fill this in
 
 
@@ -48,8 +57,8 @@ int partTwo(string in) {
 }
 
 int main() {
-    input = getInput();
-    cout << partOne(input) << endl;
-    cout << partTwo(input);
+    getInput();
+    cout << "Part 1: " << partOne() << endl;
+    cout << "Part 2: " << partTwo();
     return 0;
 }
